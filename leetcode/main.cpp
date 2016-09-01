9eab31e908ad6b0e4a528f128688975043b1bb14
Neo He <neohe2007@gmail.com>
2016-09-01 08:36:04 +0800

    init leetcode

---
+++ leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by heyong on 16/9/1.
+//  Copyright © 2016年 heyong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
